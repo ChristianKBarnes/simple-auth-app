11fd911b90a55fe01867c11cad2c0ad759123905
Qhris-tian <70176952+ChristianKBarnes@users.noreply.github.com>
2022-09-03 02:28:02 +0000

    Update database.py

--- project/app/config/database.py
+++ project/app/config/database.py
@@ -1,4 +1,7 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 db_host = os.environ.get('DB_HOST')
 db_database = os.environ.get('DB_DATABASE')
@@ -7,3 +10,5 @@ db_port = os.environ.get('DB_PORT')
 db_password = os.environ.get('DB_PASSWORD')
 
 db_url = "postgresql://{user}:{password}@{host}:{port}/{database}".format(user=db_user, password=db_password, host=db_host, port=db_port, database=db_database)
+
+db_test_url = "postgresql://{user}:{password}@{host}:{port}/{database}".format(user=db_user, password=db_password, host=db_host, port=db_port, database=db_database)
