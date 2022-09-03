f1360415b4c4d099f9de74db3eab395e4c66e0da
Qhris-tian <70176952+ChristianKBarnes@users.noreply.github.com>
2022-09-03 02:28:25 +0000

    Update main.py

--- project/app/main.py
+++ project/app/main.py
@@ -1,10 +1,17 @@
-from typing import Union
+import logging
 
 from fastapi import FastAPI
 
-app = FastAPI()
+from app.api import ping
+
+log = logging.getLogger("uvicorn")
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+def create_application() -> FastAPI:
+    application = FastAPI()
+    application.include_router(ping.router)
+
+    return application
+
+
+app = create_application()
