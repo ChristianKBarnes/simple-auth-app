18a759cd26874d0c72d3e606e94a94fc2a3f3132
Qhris-tian <70176952+ChristianKBarnes@users.noreply.github.com>
2022-09-05 08:11:13 +0000

    Update conftest.py

--- project/test/conftest.py
+++ project/test/conftest.py
@@ -1,18 +1,27 @@
-import os
-
+import email
 import pytest
+
 from starlette.testclient import TestClient
-from dotenv import load_dotenv
+from tortoise.contrib.fastapi import register_tortoise
+from faker import Faker
 
 from app.config.app import Settings, get_settings
 from app.main import create_application
 from app.config import database
+from app.api.auth import get_current_active_user
 
-load_dotenv()
+fake = Faker()
 
 
 def get_settings_override():
     return Settings(testing=1, database_url=database.db_test_url, environment="testing")
+
+
+def get_current_active_user_override():
+    return {
+        "email": fake.email(),
+        "name": fake.name(),
+    }
 
 
 @pytest.fixture(scope="module")
@@ -24,5 +33,27 @@ def test_app():
 
         # testing
         yield test_client
+
+    # tear down
+
+
+@pytest.fixture(scope="module")
+def test_app_with_db():
+    # set up
+    app = create_application()
+    app.dependency_overrides[get_settings] = get_settings_override
+    app.dependency_overrides[get_current_active_user] = get_current_active_user_override
+    register_tortoise(
+        app,
+        db_url=database.db_test_url,
+        modules={"models": ["app.models.user"]},
+        generate_schemas=True,
+        add_exception_handlers=True,
+    )
+    with TestClient(app) as test_client:
+
+        # testing
+        yield test_client
+
 
     # tear down
