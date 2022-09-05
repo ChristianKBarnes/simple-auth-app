3e5f776a1e2a2a37d353fc435a2b3e377ae101a7
Qhris-tian <70176952+ChristianKBarnes@users.noreply.github.com>
2022-09-05 08:10:21 +0000

    Update main.py

--- project/app/main.py
+++ project/app/main.py
@@ -2,7 +2,9 @@ import logging
 
 from fastapi import FastAPI
 
-from app.api import ping
+from app.api import ping, users, auth
+
+from app.db import init_db
 
 log = logging.getLogger("uvicorn")
 
@@ -10,8 +12,20 @@ log = logging.getLogger("uvicorn")
 def create_application() -> FastAPI:
     application = FastAPI()
     application.include_router(ping.router)
-
+    application.include_router(auth.router, prefix="/auth", tags=["auth"])
+    application.include_router(users.router, prefix="/users", tags=["users"])
     return application
 
 
 app = create_application()
+
+
+@app.on_event("startup")
+async def startup_event():
+    log.info("Starting up...")
+    init_db(app)
+
+
+@app.on_event("shutdown")
+async def shutdown_event():
+    log.info("Shutting down...")
