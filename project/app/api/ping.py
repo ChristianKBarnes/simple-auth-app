d683e55d43f5be39b961dcd2f7255b839d45fd9e
Qhris-tian <70176952+ChristianKBarnes@users.noreply.github.com>
2022-09-06 10:43:53 +0000

    Update ping.py

--- project/app/api/ping.py
+++ project/app/api/ping.py
@@ -1,8 +1,14 @@
 from fastapi import APIRouter, Depends
+from starlette import responses
 
 from app.config.app import Settings, get_settings
 
 router = APIRouter()
+
+
+@router.get("/")
+async def root():
+    return responses.RedirectResponse("/redoc")
 
 
 @router.get("/ping")
