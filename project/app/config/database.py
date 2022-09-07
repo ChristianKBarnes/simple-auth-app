3f3ac75a38f70fbabe77d1c853d59b36546987e6
Qhris-tian <70176952+ChristianKBarnes@users.noreply.github.com>
2022-09-07 13:57:06 +0000

    Update database.py

--- project/app/config/database.py
+++ project/app/config/database.py
@@ -3,12 +3,14 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-db_host = os.environ.get('DB_HOST')
-db_database = os.environ.get('DB_DATABASE')
-db_user = os.environ.get('DB_USER')
-db_port = os.environ.get('DB_PORT')
-db_password = os.environ.get('DB_PASSWORD')
+db_host = os.environ.get("DB_HOST")
+db_database = os.environ.get("DB_DATABASE")
+db_user = os.environ.get("DB_USER")
+db_port = os.environ.get("DB_PORT")
+db_password = os.environ.get("DB_PASSWORD")
 
-db_url = "postgres://{user}:{password}@{host}:5432/{database}".format(user=db_user, password=db_password, host=db_host, database=db_database)
+db_url = "postgres://{user}:{password}@{host}:5432/{database}".format(
+    user=db_user, password=db_password, host=db_host, database=db_database
+)
 
 db_test_url = "sqlite:///./test_db.db"
