bc07ecd4f871ab9c511ef68ab24f32dc1c081ff9
Qhris-tian <70176952+ChristianKBarnes@users.noreply.github.com>
2022-09-11 16:46:40 +0000

    Update test_users.py

--- project/test/feature/test_users.py
+++ project/test/feature/test_users.py
@@ -1,12 +1,24 @@
+import datetime, pytest
+from typing import List
+
 from faker import Faker
+from pydantic import EmailStr
+
+from app.models.user import User
 
 fake = Faker()
 
 
-def test_get_request_returns_405(test_app_with_db):
-    """endpoint does only expect a post request"""
-    response = test_app_with_db.get("/users/")
-    assert response.status_code == 405
+@pytest.fixture
+async def create_user(email: EmailStr = None, name: str = None) -> User:
+    """create a user in the db"""
+    email = fake.email() if email is None else email
+    name = fake.name() if name is None else name
+
+    user = User(email=email, name=name, password=fake.password())
+    await user.save()
+
+    return user
 
 
 def test_post_request_with_improper_body_returns_422(test_app_with_db):
@@ -15,18 +27,16 @@ def test_post_request_with_improper_body_returns_422(test_app_with_db):
     assert response.status_code == 422
 
 
-def test_post_request_with_existing_information_returns_422(test_app_with_db):
+@pytest.mark.parametrize("anyio_backend", ["asyncio"])
+async def test_post_request_with_existing_information_returns_422(
+    test_app_with_db, anyio_backend, create_user
+):
     """expects users to be unique and non-existent"""
-    email = fake.email()
-    response = test_app_with_db.post(
-        "/users/",
-        json={"name": fake.name(), "email": email, "password": fake.password()},
-    )
-    assert response.status_code == 201
+    user = create_user
 
     response = test_app_with_db.post(
         "/users/",
-        json={"name": fake.name(), "email": email, "password": fake.password()},
+        json={"name": fake.name(), "email": user.email, "password": fake.password()},
     )
     assert response.status_code == 422
 
@@ -37,3 +47,45 @@ def test_post_request_with_proper_body_returns_201(test_app_with_db):
         json={"name": fake.name(), "email": fake.email(), "password": fake.password()},
     )
     assert response.status_code == 201
+
+
+def test_get_returns_all_users(test_app_with_db):
+    response = test_app_with_db.get("/users/")
+
+    assert response.status_code == 200
+    assert isinstance(response.json(), List)
+
+
+@pytest.mark.parametrize("anyio_backend", ["asyncio"])
+async def test_get_request_with_id_returns_user(
+    test_app_with_db, anyio_backend, create_user
+):
+    user = create_user
+
+    response = test_app_with_db.get("/users/{id}".format(id=user.id))
+    data = response.json()
+
+    assert data["id"] == user.id
+    assert data["email"] == user.email
+    assert data["name"] == user.name
+    assert response.status_code == 200
+
+
+@pytest.mark.parametrize("anyio_backend", ["asyncio"])
+def test_delete_request_deletes_user(test_app_with_db, anyio_backend, create_user):
+    user = create_user
+
+    response = test_app_with_db.delete("/users/{id}".format(id=user.id))
+    assert response.status_code == 204
+
+    response = test_app_with_db.get("/users/{id}".format(id=user.id))
+    assert response.json() == {"detail": "User Not Found"}
+    assert response.status_code == 404
+
+
+def test_delete_request_works_for_only_existing_users(test_app_with_db):
+
+    response = test_app_with_db.delete("/users/0")
+
+    assert response.json() == {"detail": "User Not Found"}
+    assert response.status_code == 404
