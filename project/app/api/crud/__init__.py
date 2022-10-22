211ca1671bdcd8aa7b584cfb74d06981414ef38e
Qhris-tian <70176952+ChristianKBarnes@users.noreply.github.com>
2022-10-22 11:54:01 +0000

    Update __init__.py

--- project/app/api/crud/__init__.py
+++ project/app/api/crud/__init__.py
@@ -3,7 +3,9 @@ from tortoise import Tortoise
 from tortoise.contrib.pydantic import pydantic_model_creator
 
 from app.models.student import Student
+from app.models.student_attendance import StudentAttendance
 
 Tortoise.init_models(database.MODELS, "models")
 
 Student_Pydantic = pydantic_model_creator(Student)
+Attendance_Pydantic = pydantic_model_creator(StudentAttendance)
