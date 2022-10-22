718ee930e7ee74dcfef71e700b252e68ccecf86a
Qhris-tian <70176952+ChristianKBarnes@users.noreply.github.com>
2022-10-22 11:53:47 +0000

    Update api.py

--- project/app/router/api.py
+++ project/app/router/api.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends
 
-from app.api import ping, users, auth, students, guardians
+from app.api import ping, users, auth, students, guardians, attendance
 from app.providers.AuthenticationProvider import AuthenticationProvider
 
 api_router = APIRouter()
@@ -10,4 +10,5 @@ api_router.include_router(ping.router)
 api_router.include_router(auth.router, prefix="/auth", tags=["auth"])
 api_router.include_router(users.router, prefix="/users", tags=["users"])
 api_router.include_router(students.router, prefix="/students", tags=["students"])
+api_router.include_router(attendance.router, prefix="/attendance", tags=["attendance"])
 api_router.include_router(guardians.router, prefix="/guardians", tags=["guardians"])
