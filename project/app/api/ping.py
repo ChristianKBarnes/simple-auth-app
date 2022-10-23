e8ad33e5d91b24aee92e66909f7e2f87db723116
Qhris-tian <70176952+ChristianKBarnes@users.noreply.github.com>
2022-10-23 03:59:58 +0000

    Update ping.py

--- project/app/api/ping.py
+++ project/app/api/ping.py
@@ -7,12 +7,12 @@ from app.config.app import Settings, get_settings
 router = APIRouter()
 
 
-@router.get("/", response_class=RedirectResponse)
+@router.get("/", response_class=RedirectResponse, include_in_schema=False)
 async def root():
     return responses.RedirectResponse("/redoc")
 
 
-@router.get("/ping")
+@router.get("/ping", include_in_schema=False)
 async def pong(settings: Settings = Depends(get_settings)):
     return {
         "ping": "pong!",
