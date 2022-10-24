b3e0daca28f5a2e4b84cde6f130040f021874492
Qhris-tian <70176952+ChristianKBarnes@users.noreply.github.com>
2022-10-24 10:04:32 +0000

    Update app.py

--- project/app/config/app.py
+++ project/app/config/app.py
@@ -1,10 +1,10 @@
 import logging
 import os
+from typing import Any, Dict
 
 from dotenv import load_dotenv
 from functools import lru_cache
-from fastapi import Path
-from pydantic import BaseSettings
+from pydantic import BaseSettings, validator
 from fastapi_mail import ConnectionConfig
 
 from app.config import database
@@ -24,21 +24,19 @@ class Settings(BaseSettings):
     testing: bool = os.getenv("TESTING", 0)
     database_url = database.db_url
 
-    # MAIL_TEMPLATE_FOLDER: str = "app/email-templates/build"
-
     email_configuration = ConnectionConfig(
         MAIL_USERNAME=os.getenv("MAIL_USERNAME"),
         MAIL_PASSWORD=os.getenv("MAIL_PASSWORD"),
         MAIL_FROM=os.getenv("MAIL_FROM"),
         MAIL_PORT=os.getenv("MAIL_PORT"),
         MAIL_SERVER=os.getenv("MAIL_SERVER"),
+        TEMPLATE_FOLDER=os.getenv("MAIL_TEMPLATE_FOLDELR"),
         MAIL_TLS=True,
         MAIL_SSL=False,
-        # TEMPLATE_FOLDER=MAIL_TEMPLATE_FOLDER,
     )
 
 
-@lru_cache()  # pragma: no cover
+@lru_cache()
 def get_settings() -> BaseSettings:
     log.info("Loading config settings from the environment...")
     return Settings()
@@ -49,4 +47,4 @@ app_hash_algorithm: str = os.getenv("APP_HASH_ALGORITHM", None)
 app_access_token_expire_minutes: float = os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 30)
 
 
-settings = Settings()
+settings = get_settings()
